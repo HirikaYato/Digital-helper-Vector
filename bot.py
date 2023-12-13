072ba93da28dc9f2fb2b15cfd54b6834e55531c3
MAJHQO <thehqo1337@gmail.com>
2023-12-13 12:26:39 +0300

    Bot_0.0

---
+++ bot.py
@@ -0,0 +1,19 @@
+import vk_api as vk_api
+import random
+import json
+import time
+
+from vk_api.bot_longpoll import VkBotEventType,VkBotLongPoll,VkBotMessageEvent,VkBotEvent
+from vk_api.requests_pool import VkRequestsPool,RequestResult
+
+
+def main():
+    print("BOT STARTED")
+
+    print('Launch scenary: ', main.__name__)
+
+    
+
+
+
+main()
